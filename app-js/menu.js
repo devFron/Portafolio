9f6e61fc0580e7a2f089f240ca5f199646e7668c
jose vit <josegabriel.vit@gmail.com>
2021-03-07 14:26:11 -0600

    se capturo el evento y se hizo un toggle de clases en el menu

--- app-js/menu.js
+++ app-js/menu.js
@@ -1,5 +1,11 @@
 export const Menu = ( )=>{
     let $menu = document.querySelector('#menu')
     let $menuIcon = document.querySelector('.menu__icon')
-    
+
+    document.addEventListener('click',(e)=>{
+        if(e.target === $menuIcon){
+            $menu.classList.toggle('menu__active')
+        }
+    })
+
 }
