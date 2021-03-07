66e4e8398ddcdc874ede0903459f7498cf36944a
jose vit <josegabriel.vit@gmail.com>
2021-03-07 14:44:10 -0600

    en el html se mobio el modal de lugar y se reacomodaron las cosas en el css y en el main js se importo la funcion a ejecutar y el menu.js se cambio el tipo d capura del el menu y modal y se hizo un toogle en el moda

--- app-js/menu.js
+++ app-js/menu.js
@@ -1,10 +1,11 @@
 export const Menu = ( )=>{
-    let $menu = document.querySelector('#menu')
+    let $menu = document.querySelector('.menu')
     let $menuIcon = document.querySelector('.menu__icon')
-
+    let $modalColor = document.querySelector('.modal-color')
     document.addEventListener('click',(e)=>{
         if(e.target === $menuIcon){
             $menu.classList.toggle('menu__active')
+            $modalColor.classList.toggle('modal-color__active')
         }
     })
 
