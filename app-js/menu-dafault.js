5a3412b782cac6d25bcdd44ee9ab8f756434e601
jose vit <josegabriel.vit@gmail.com>
2021-03-08 15:50:52 -0600

    se hizieron las siguientes cards que aparecen por default

--- app-js/menu-dafault.js
+++ app-js/menu-dafault.js
@@ -107,8 +107,108 @@ export const  menuDefaul = ()=>{
     $backCardOfertText.appendChild($backCardPrice)
 
 
+    let $backCardOfertTwo = document.createElement('article')
+    $backCardOfertTwo.classList.add('back-card-ofert')
+    
+    let $backCardOfertTextTwo = document.createElement('div')
+    $backCardOfertTextTwo.classList.add('back-card__text__ofert')
+
+    let $backCardTitlesTwo = document.createElement('div')
+    $backCardTitlesTwo.classList.add('back-card__titles')
+
+    let $circleTwo = document.createElement('div')
+    $circleTwo.classList.add('circle')
+
+    let $circleIconTwo = document.createElement('i')
+    $circleIconTwo.classList.add('fas')
+    $circleIconTwo.classList.add('fa-check')
+    $circleIconTwo.classList.add('circle-icon')
+    $circleTwo.appendChild($circleIconTwo)
+
+    let $backCardOfertTitleTwo = document.createElement('p')
+    $backCardOfertTitleTwo.classList.add('back-card__title__ofert')
+    $backCardOfertTitleTwo.textContent = `Black Edition`
+
+    let $backCardOfertPriceTwo = document.createElement('p')
+    $backCardOfertPriceTwo.classList.add('back-card__price')
+    $backCardOfertPriceTwo.textContent = `Pledge $75 or more`
+
+    let $backCardPriceTwo = document.createElement('p')
+    $backCardPriceTwo.classList.add('back-card__price__number')
+    $backCardPriceTwo.textContent = '101'
+
+    let $backCardPriceOffsetTwo = document.createElement('span')
+    $backCardPriceOffsetTwo.classList.add('back-card__title__number__ofset')
+    $backCardPriceOffsetTwo.textContent = 'left'
+    $backCardPriceTwo.appendChild($backCardPriceOffsetTwo)
+
+    let $backCardOfertDescriptionTwo = document.createElement('p')
+    $backCardOfertDescriptionTwo.classList.add('back-card__description___ofert')
+    $backCardOfertDescriptionTwo.textContent = `You get an ergonomic stand made of natural bamboo.You've helped us launch our promotional campaign,and you'llbe added yo a special backer member list.`
+
+
+    $backCardOfertTwo.appendChild($backCardOfertTextTwo)
+    $backCardOfertTwo.appendChild($backCardOfertDescriptionTwo)
+    $backCardOfertTextTwo.appendChild($circleTwo)
+    $backCardOfertTextTwo.appendChild($backCardTitlesTwo)
+    $backCardTitlesTwo.appendChild($backCardOfertTitleTwo)
+    $backCardTitlesTwo.appendChild($backCardOfertPriceTwo)
+    $backCardOfertTextTwo.appendChild($backCardPriceTwo)
+
+    // ------
+    let $backCardOfertThree = document.createElement('article')
+    $backCardOfertThree.classList.add('back-card-ofert')
+    
+    let $backCardOfertTextThree = document.createElement('div')
+    $backCardOfertTextThree.classList.add('back-card__text__ofert')
+
+    let $backCardTitlesThree = document.createElement('div')
+    $backCardTitlesThree.classList.add('back-card__titles')
+
+    let $circleThree = document.createElement('div')
+    $circleThree.classList.add('circle')
+
+    let $circleIconThree = document.createElement('i')
+    $circleIconThree.classList.add('fas')
+    $circleIconThree.classList.add('fa-check')
+    $circleIconThree.classList.add('circle-icon')
+    $circleThree.appendChild($circleIconThree)
+
+    let $backCardOfertTitleThree = document.createElement('p')
+    $backCardOfertTitleThree.classList.add('back-card__title__ofert')
+    $backCardOfertTitleThree.textContent = `Black Edition Stand`
+
+    let $backCardOfertPriceThree = document.createElement('p')
+    $backCardOfertPriceThree.classList.add('back-card__price')
+    $backCardOfertPriceThree.textContent = `Pledge $75 or more`
+
+    let $backCardPriceThree = document.createElement('p')
+    $backCardPriceThree.classList.add('back-card__price__number')
+    $backCardPriceThree.textContent = '101'
+
+    let $backCardPriceOffsetThree = document.createElement('span')
+    $backCardPriceOffsetThree.classList.add('back-card__title__number__ofset')
+    $backCardPriceOffsetThree.textContent = 'left'
+    $backCardPriceThree.appendChild($backCardPriceOffsetThree)
+
+    let $backCardOfertDescriptionThree = document.createElement('p')
+    $backCardOfertDescriptionThree.classList.add('back-card__description___ofert')
+    $backCardOfertDescriptionThree.textContent = `You get an ergonomic stand made of natural bamboo.You've helped us launch our promotional campaign,and you'llbe added yo a special backer member list.`
+
+
+    $backCardOfertThree.appendChild($backCardOfertTextThree)
+    $backCardOfertThree.appendChild($backCardOfertDescriptionThree)
+    $backCardOfertTextThree.appendChild($circleThree)
+    $backCardOfertTextThree.appendChild($backCardTitlesThree)
+    $backCardTitlesThree.appendChild($backCardOfertTitleThree)
+    $backCardTitlesThree.appendChild($backCardOfertPriceThree)
+    $backCardOfertTextThree.appendChild($backCardPriceThree)
+
+
     $backCards.appendChild($backCard)
     $backCards.appendChild($backCardOfert)
+    $backCards.appendChild($backCardOfertTwo)
+    $backCards.appendChild($backCardOfertThree)
     Fragment.appendChild(modalDefaul)
     $body.appendChild(Fragment)
     
