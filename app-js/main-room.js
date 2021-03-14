6c99a09091b269530889d2c92f23392dda3cf081
jose vit <josegabriel.vit@gmail.com>
2021-03-13 20:27:38 -0600

    se creo el toggle del menu y se importo en el main

--- app-js/main-room.js
+++ app-js/main-room.js
@@ -1,7 +1,8 @@
 import { Slider, SliderResponsive } from "./slider-room.js";
-
+import { Menu } from "./menu-room.js";
 
 document.addEventListener('DOMContentLoaded',(e)=>{
     Slider()
     SliderResponsive()
+    Menu()
 })
