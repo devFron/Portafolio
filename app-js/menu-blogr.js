8bf40ed1549eef60a099736c36d2e0d3fbfb896e
jose vit <josegabriel.vit@gmail.com>
2021-03-15 14:10:02 -0600

    se acomodaron unas clases y se hizo el toggle del menu y el icon de hamburguesa

--- app-js/menu-blogr.js
+++ app-js/menu-blogr.js
@@ -3,4 +3,18 @@ export const Menu = ()=>{
     let $hamburguerIcon = document.querySelector('.hambuguer-icon')
     let $menuContact = document.querySelectorAll('#menu-contact')
 
+    document.addEventListener('click',(e)=>{
+
+        if (e.target === $hamburguerIcon) {
+            $nav.classList.toggle('nav__enabled') 
+        }
+        if ($nav.classList.contains('nav__enabled')) {
+            $hamburguerIcon.classList.remove('fa-bars')
+            $hamburguerIcon.classList.add('fa-times')
+        }else{
+            $hamburguerIcon.classList.add('fa-bars')
+            $hamburguerIcon.classList.remove('fa-times')
+            
+        }
+    })
 }
