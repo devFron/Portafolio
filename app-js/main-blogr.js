a64ecd6234d300ab31364e9e3595bfeffa1baf0d
jose vit <josegabriel.vit@gmail.com>
2021-03-15 14:00:30 -0600

    se enlazo el main-blogr.js con el html correspondinete

---
+++ app-js/main-blogr.js
@@ -0,0 +1,5 @@
+import { Menu } from "./menu-blogr.js";
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+    Menu()
+})
---
+++ app-js/menu-blogr.js
@@ -0,0 +1,6 @@
+export const Menu = ()=>{
+    let $nav = document.querySelector('#nav')
+    let $hamburguerIcon = document.querySelector('.hambuguer-icon')
+    let $menuContact = document.querySelectorAll('#menu-contact')
+
+}
