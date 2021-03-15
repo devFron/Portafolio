5ebc5f7560c7c4d36a4b41208ce6fd55932207cc
jose vit <josegabriel.vit@gmail.com>
2021-03-15 14:35:20 -0600

    se hizo el toggle de para aparecer el submenu

--- app-js/menu-blogr.js
+++ app-js/menu-blogr.js
@@ -1,8 +1,10 @@
 export const Menu = ()=>{
     let $nav = document.querySelector('#nav')
     let $hamburguerIcon = document.querySelector('.hambuguer-icon')
-    let $menuContact = document.querySelectorAll('#menu-contact')
-
+    let $menuContact = document.querySelectorAll('.menu__contact')
+    let $menuIconArrow = document.querySelectorAll('.menu__icon')
+    let $menuLink = document.querySelectorAll('.menu__link')
+    console.log($menuLink);
     document.addEventListener('click',(e)=>{
 
         if (e.target === $hamburguerIcon) {
@@ -15,6 +17,30 @@ export const Menu = ()=>{
             $hamburguerIcon.classList.add('fa-bars')
             $hamburguerIcon.classList.remove('fa-times')
             
+        }
+        switch (e.target) {
+
+            case $menuIconArrow[0]:
+                $menuContact[0].classList.toggle('menu__contact__enabled')
+                e.preventDefault()
+            break;
+                
+            case $menuIconArrow[1]:
+                $menuContact[1].classList.toggle('menu__contact__enabled')
+                e.preventDefault()
+            break;
+                
+            case $menuIconArrow[2]:
+                $menuContact[2].classList.toggle('menu__contact__enabled') 
+                e.preventDefault()
+            break;
+        
+            default:
+            break;
+        }
+
+        if (e.target === $menuLink[0] ||e.target === $menuLink[1]||e.target === $menuLink[2]) {
+            e.preventDefault()
         }
     })
 }
