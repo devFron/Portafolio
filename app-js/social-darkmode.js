94384cde827f9d832d478da6f8bfeaa33abda9bd
jose vit <josegabriel.vit@gmail.com>
2021-03-20 22:52:18 -0600

    se enlazo el script co el index y se creo la funcion y las variables a utilizar

---
+++ app-js/social-darkmode.js
@@ -0,0 +1,15 @@
+const  DarkMode = ()=>{
+    let $body = document.querySelector('body')
+    let $headerTitle = document.querySelector('#header-title')
+    let $headerFollowers = document.querySelector('#header-followers')
+    let $buttonDark = document.querySelector('#button')
+    let $buttonDarkIcon = document.querySelector('#button-cirle')
+    let $socialMediaCard = document.querySelectorAll('.social-media__card')
+    let $socialMediaCardCounter = document.querySelectorAll('.social-media__card__followers__plus__description')
+    let $overviewTitle = document.querySelector('#overview-title')
+    let $overviewCardsCardData = document.querySelectorAll('.overview__cards__card__data')
+    let $overviewDataNumbersView = document.querySelectorAll('.overview__cards__card__data__views__number__dark')
+    let $overviewDataNumbersLikes = document.querySelectorAll('.overview__cards__card__data__likes__number')
+    
+}
+DarkMode()
