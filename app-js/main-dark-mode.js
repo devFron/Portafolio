2fd16306d8f1fcf33ea2c1624fb8138d0037207e
jose vit <josegabriel.vit@gmail.com>
2021-03-24 15:46:16 -0600

    se agregaron la variables a usar

--- app-js/main-dark-mode.js
+++ app-js/main-dark-mode.js
@@ -1,3 +1,12 @@
 export const DarkMode = () =>{
+    let $body = document.querySelector('body')
+    let $header = document.querySelector('#header')
+    let $menuLink = document.querySelectorAll('.menu__link')
+    let $circle = document.querySelector('.circle')
+    let $main = document.querySelector('#main')
+    let assistantBody = document.querySelector('.assistant__body')
+    let $frontMentorLink = document.querySelector('.front-met')
+    let $sliderButtons = document.querySelectorAll('.assistant__buttons__button')
+    let $sliderButtonsActive = document.querySelectorAll('.assistant__buttons__slider__active')
 
 }
