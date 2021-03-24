5cf557639ef546f1f74d4202e5fb97aae80d880d
jose vit <josegabriel.vit@gmail.com>
2021-03-24 15:37:03 -0600

    se creo la funcion a jecturar y se import en el modulo js"

--- app-js/main-index.js
+++ app-js/main-index.js
@@ -1,8 +1,9 @@
 import { assistant, assistantSlider } from "./asistente-index.js";
-
+import { DarkMode } from "./main-dark-mode.js";
 
 
 document.addEventListener('DOMContentLoaded',(e)=>{
     assistant()
     assistantSlider()
+    DarkMode()
 })
