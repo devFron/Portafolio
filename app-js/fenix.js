79652130f9d0baaa6e9ad41a6ea0f2f2d2f7dffd
Dev Front <developermater26@gmail.com>
2021-04-14 18:56:05 -0600

    se creo una nueva instancia del objeto XMLHttpRequest se manejron todas las validaciones y ejecuciones

--- app-js/fenix.js
+++ app-js/fenix.js
@@ -1,4 +1,22 @@
-const Fenix =( )=>{
+const Fenix =(options)=>{
 
+    const xhr = new XMLHttpRequest()
+
+    let {url,succes,error,el} = options
+
+    xhr.addEventListener('readystatechange',(e)=>{
+        if(xhr.readyState !== 4) return
+
+        if(xhr.status >= 200 && xhr.status < 300){
+            succes(xhr,el)
+        }else{
+            error(xhr,el)
+        }
+    })
+
+    xhr.open('GET',url)
+    xhr.setRequestHeader('Content-Type','text/html')
+    xhr.send()
 }
-document.addEventListener('DOMContentLoaded',(e)=>Fenix())
+
+document.addEventListener('DOMContentLoaded',(e)=> Fenix())
