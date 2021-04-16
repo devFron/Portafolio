bb112583a914c57e276d4ddfe7ff5dc994e1f586
Dev Front <developermater26@gmail.com>
2021-04-15 22:02:23 -0600

    se importo la funcio para ejecutarse

--- app-js/main-index.js
+++ app-js/main-index.js
@@ -1,9 +1,10 @@
 import { assistant, assistantSlider } from "./asistente-index.js";
+import { Menu } from "./main-menu.js";
 import { DarkMode } from "./main-dark-mode.js";
-
 
 document.addEventListener('DOMContentLoaded',(e)=>{
     assistant()
     assistantSlider()
+    Menu()
     DarkMode()
 })
