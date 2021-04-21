8585fe01b52f3c3118bf4c50989a27bd8a54108c
Dev Front <developermater26@gmail.com>
2021-04-20 19:48:25 -0600

    se importo la funcion del modo oscuro de sobre

--- app-js/main-sobre.js
+++ app-js/main-sobre.js
@@ -1,4 +1,6 @@
 import { Menu } from "./main-menu.js";
+import { DarkModeSobre} from "./dark-mode-sobre.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     Menu()
+    DarkModeSobre()
 })
