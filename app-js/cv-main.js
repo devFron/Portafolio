6054976ac478bea3fb8c5bab32b5f83c9883cce4
Dev Front <developermater26@gmail.com>
2021-04-29 16:17:04 -0600

    se hizo que se impimiera el cv

--- app-js/cv-main.js
+++ app-js/cv-main.js
@@ -3,4 +3,16 @@ import { DarkModeCV } from "./cv-dark-mode.js";
 document.addEventListener('DOMContentLoaded',(e)=>{ 
     Menu()
     DarkModeCV()
+    document.addEventListener('click',(e)=>{
+        if(e.target.matches('.my__button') || e.target.matches('.my__button-two')){
+            window.open('../resources/PDF/CV.pdf')
+        }
+    })
+
+    document.addEventListener('keydown',(e)=>{
+        e.preventDefault()
+        if(e.key === 'p' && e.ctrlKey){
+            window.open('../resources/PDF/CV.pdf')
+        }
+    })
 })
