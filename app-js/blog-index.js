fd118a2865772440d4e05e69fb7108244acb074f
Dev Front <developermater26@gmail.com>
2021-04-29 18:26:13 -0600

    se comento la importacion del archivo import-post.js y la funcion ImportPost() y se agregao un listener para abrir el post en otra pestaña dinamicamente

--- app-js/blog-index.js
+++ app-js/blog-index.js
@@ -1,6 +1,12 @@
-import { ImportPost } from "./import-post.js";
+// import { ImportPost } from "./import-post.js";
 import { Menu } from "./main-menu.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
-    ImportPost()
+    // ImportPost()
     Menu()
+
+    document.addEventListener('click',(e)=>{
+        if(e.target.matches('.post__image')){
+            window.open(`${e.target.dataset.link}`)
+        }
+    })
 })
