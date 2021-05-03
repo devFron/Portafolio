96df9581d4e5b1ff6aa1e2fd48d1dd0f030db844
Dev Front <developermater26@gmail.com>
2021-05-03 16:37:29 -0600

    se cambio de nombre a el arhcivo de  mensaje de thansh-suport y se imporot con el nombre nuevo

--- app-js/crowdfunding_module.js
+++ app-js/crowdfunding_module.js
@@ -1,6 +1,6 @@
 import { Menu } from "./crowdfunding_menu.js";
 import { menuDefaul } from "./crowdfunding_menu-dafault.js";
-import { Thanks } from "./thanks-suport.js";
+import { Thanks } from "./crowdfunding_thanks-suport.js";
 import { cardDisable } from "./card-disable.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     Menu()
