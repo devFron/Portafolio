3574e666ae25cac42d7af9c9d3aa445d9a30a936
Dev Front <developermater26@gmail.com>
2021-05-03 16:33:19 -0600

    se cambio el nombre del archivo de menu de crowdfunding y se importo con el nombre correcto

--- app-js/crowdfunding_module.js
+++ app-js/crowdfunding_module.js
@@ -1,4 +1,4 @@
-import { Menu } from "./menu.js";
+import { Menu } from "./crowdfunding_menu.js";
 import { menuDefaul } from "./menu-dafault.js";
 import { Thanks } from "./thanks-suport.js";
 import { cardDisable } from "./card-disable.js";
