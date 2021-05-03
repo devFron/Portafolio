22fb00e31192fae64f0501b88afd162b4ff09814
Dev Front <developermater26@gmail.com>
2021-05-03 16:18:00 -0600

    se cambio el nombre del archivo que contiene el modo oscuro de recursos y se cambio la direccion la importacion de dicho archivo

--- app-js/recursos_module.js
+++ app-js/recursos_module.js
@@ -1,5 +1,5 @@
 import { Menu } from "./index_menu.js";
-import { DarkModeRecursos } from "./recursos-dark-mode.js";
+import { DarkModeRecursos } from "./recursos_dark-mode.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     DarkModeRecursos()
     Menu()
