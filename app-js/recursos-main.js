859811cd3b61205772f22e52290d7cf05cb1e990
Dev Front <developermater26@gmail.com>
2021-05-03 15:48:17 -0600

    se cambio el nombre de main-menu.js por index_menu.js y se arreglo en la importacion

--- app-js/recursos-main.js
+++ app-js/recursos-main.js
@@ -1,4 +1,4 @@
-import { Menu } from "./main-menu.js";
+import { Menu } from "./index_menu.js";
 import { DarkModeRecursos } from "./recursos-dark-mode.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     DarkModeRecursos()
