31865b394221010d1256cca92b27e2e5b43626a1
Dev Front <developermater26@gmail.com>
2021-05-03 16:35:32 -0600

    se cambio el nombre de el archivo del menu por default y se redirecciono la importacion

--- app-js/crowdfunding_module.js
+++ app-js/crowdfunding_module.js
@@ -1,5 +1,5 @@
 import { Menu } from "./crowdfunding_menu.js";
-import { menuDefaul } from "./menu-dafault.js";
+import { menuDefaul } from "./crowdfunding_menu-dafault.js";
 import { Thanks } from "./thanks-suport.js";
 import { cardDisable } from "./card-disable.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
