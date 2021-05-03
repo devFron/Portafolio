6f8022109275fcabdb501b9302d23235c7085f56
Dev Front <developermater26@gmail.com>
2021-05-03 16:39:34 -0600

    se cambio de nombre a el arhcivo de card-disable y  y se importo con el nombre nuevo

--- app-js/crowdfunding_module.js
+++ app-js/crowdfunding_module.js
@@ -1,7 +1,7 @@
 import { Menu } from "./crowdfunding_menu.js";
 import { menuDefaul } from "./crowdfunding_menu-dafault.js";
 import { Thanks } from "./crowdfunding_thanks-suport.js";
-import { cardDisable } from "./card-disable.js";
+import { cardDisable } from "./crowdfunding_card-disable.js";
 document.addEventListener('DOMContentLoaded',(e)=>{
     Menu()
     menuDefaul()
