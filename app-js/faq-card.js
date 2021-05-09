ffd87291e986621da44bb8bb36ea4d71a761017a
Dev Front <developermater26@gmail.com>
2021-05-08 21:45:44 -0600

    se agrego el toggle para mostrar el texto y el transform de la flecha

--- app-js/faq-card.js
+++ app-js/faq-card.js
@@ -7,6 +7,32 @@ const ImageResize =( )=>{
         $imagetwo.src = '../resources/images/faq-card/bg-pattern-desktop.svg'
     }
 }
+const listToggle = () =>{
+
+    let $iconImage = document.querySelectorAll('.faq-list__list__itme__icon')
+    let $cardDescription = document.querySelectorAll('.faq-list__list__item__description')
+    let i  = 0
+
+    $iconImage.forEach(el => {
+        el.dataset.index = `${i++}`
+    });
+    
+    if(i > 0){
+        i = 0
+    }
+
+    $cardDescription.forEach(el => {
+        el.dataset.index = `${i++}`
+    });
+
+    document.addEventListener('click',(e)=>{
+        if(e.target.matches('.faq-list__list__itme__icon')){
+            $cardDescription[e.target.dataset.index].classList.toggle('faq-list__list__item__description__enabled')
+            $iconImage[e.target.dataset.index].classList.toggle('faq-list__list__itme__icon__click')
+        }
+    })    
+}
 document.addEventListener('DOMContentLoaded',(e)=>{
     ImageResize()
+    listToggle()
 })
