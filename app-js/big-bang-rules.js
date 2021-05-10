d69a4002505d2f9c2aef0520946eb9ba85e050fd
Dev Front <developermater26@gmail.com>
2021-05-09 19:48:57 -0600

    se creo la funcion para el toogle de rules

---
+++ app-js/big-bang-rules.js
@@ -0,0 +1,13 @@
+export const Rules = ()=>{
+    let $boxBlack = document.querySelector('.box-shadow')
+    let $rulesImages = document.querySelector('.rules__images__box')
+
+    document.addEventListener('click',(e)=>{
+        if(e.target.matches('.rules__button') || e.target.matches('.rules__button__text')){
+           $rulesImages.classList.add('rules__images__box__enabled') 
+        }
+        if(e.target.matches('.rules__image__closed')){
+            $rulesImages.classList.remove('rules__images__box__enabled') 
+        }
+    })
+}
