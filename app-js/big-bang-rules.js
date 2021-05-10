73f9dd67ce39b979c941ec831826628d96216f62
Dev Front <developermater26@gmail.com>
2021-05-09 19:51:15 -0600

    se agrego toggle para el box-shadow

--- app-js/big-bang-rules.js
+++ app-js/big-bang-rules.js
@@ -5,9 +5,11 @@ export const Rules = ()=>{
     document.addEventListener('click',(e)=>{
         if(e.target.matches('.rules__button') || e.target.matches('.rules__button__text')){
            $rulesImages.classList.add('rules__images__box__enabled') 
+           $boxBlack.classList.add('box-shadow__enabled')
         }
         if(e.target.matches('.rules__image__closed')){
             $rulesImages.classList.remove('rules__images__box__enabled') 
+            $boxBlack.classList.remove('box-shadow__enabled')
         }
     })
 }
