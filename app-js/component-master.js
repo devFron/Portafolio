02b4ab0b554e252426619deb7ea7902d1d98413f
Dev Front <developermater26@gmail.com>
2021-05-15 18:45:44 -0600

    se agrego el toggle para poder aparecer y desaparecer el sharebar

--- app-js/component-master.js
+++ app-js/component-master.js
@@ -1,4 +1,13 @@
 const ShareSocialMedia = ( )=>{
+    const $shareBar = document.querySelector('.article__user__share__section')
 
+    document.addEventListener('click',(e)=>{
+
+        if(e.target.matches('.article__user__share__icon__share')){
+            $shareBar.classList.toggle('article__user__share__section__enabled')
+        }else if (e.target.matches('.article__user__share__section__social-media__icon__share')) {
+            $shareBar.classList.remove('article__user__share__section__enabled')
+        }
+    })
 }
 document.addEventListener('DOMContentLoaded',(e)=> ShareSocialMedia())
