d5f1c0287a2dac53193d704f149de603adad3790
Dev Front <developermater26@gmail.com>
2021-08-28 16:39:06 -0600

    se imporot el componente de targeta de presentacion y se agrego como un hijo de Fragment

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import Header from './app-js/components/header.js'
+import PresentationCard from './app-js/components/presentation_card.js'
 const App =()=>{
     const $root = document.getElementById('root')
     const $Fragment = document.createDocumentFragment()
@@ -6,8 +7,8 @@ const App =()=>{
 
 
 
-    
     $Fragment.appendChild(Header())
+    $Fragment.appendChild(PresentationCard())
     $root.appendChild($Fragment)
 }
 export default App
