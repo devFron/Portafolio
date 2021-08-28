f2e7dbff4862fa2ed7ea899075c0cc6a0c95f85a
Dev Front <developermater26@gmail.com>
2021-08-28 12:49:25 -0600

    1.se imporot el componente header 2. se creo un fragmento que se le agregaran como hijo todos los componentes 3. el fragmento se agrega como hijo a el root

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
+import Header from './app-js/components/header.js'
 const App =()=>{
+    const $root = document.getElementById('root')
+    const $Fragment = document.createDocumentFragment()
 
+
+
+
+    
+    $Fragment.appendChild(Header())
+    $root.appendChild($Fragment)
 }
 export default App
