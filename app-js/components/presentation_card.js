35cb118302ba122b61ede593880540862789e4b6
Dev Front <developermater26@gmail.com>
2021-08-28 16:38:12 -0600

    se creo un componente para card de prensentacion

---
+++ app-js/components/presentation_card.js
@@ -0,0 +1,6 @@
+const PresentationCard = ()=>{
+    const $presentationCard = document.createElement('section')
+    $presentationCard.classList.add('presentation-card')
+    return $presentationCard
+}
+export default PresentationCard
