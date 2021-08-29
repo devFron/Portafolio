9b2606c872fc825fcca45b5a52e46b3cc2f50311
Dev Front <developermater26@gmail.com>
2021-08-29 12:26:14 -0600

    se creo el cuerpo del componente de cards y de retoryno

--- app-js/components/card_skills.js
+++ app-js/components/card_skills.js
@@ -1,2 +1,22 @@
-const CardSkills = ()=>{}
+const CardSkills = (props)=>{
+    let {image,nameProyect,link} = props
+    const $killBox = document.createElement('article')
+    const $killImage = document.createElement('img')
+    const $killLink = document.createElement('a')
+
+    $killImage.src = image
+    $killImage.alt = nameProyect
+    $killLink.href = link
+    $killLink.textContent = nameProyect
+
+    $killBox.classList.add('kill__box')
+    $killImage.classList.add('kill__image')
+    $killLink.classList.add('kill__link')
+
+    $killBox.appendChild($killImage)
+    $killBox.appendChild($killLink)
+
+
+    return $killBox
+}
 export default CardSkills
