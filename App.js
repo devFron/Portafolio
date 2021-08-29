e6a9181a7fc9bf04d0a606e090456df581e84f97
Dev Front <developermater26@gmail.com>
2021-08-29 15:41:39 -0600

    se eliminaro las incersiones de el personal card,proyects y skills

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import Header from './app-js/components/header.js'
-import PresentationCard from './app-js/components/presentation_card.js'
-import GetProyects from './app-js/helpers/get_proyects.js'
+import Router from './app-js/helpers/router.js'
 const App =()=>{
+
     const $root = document.getElementById('root')
+    $root.innerHTML = ''
     const $Fragment = document.createDocumentFragment()
     const $mainHome = document.createElement('section')
-    const $ProyectsBox = document.createElement('section')
-    const $SkillsBox = document.createElement('section')
+    
 
     $mainHome.classList.add('main__box')
 
     $Fragment.appendChild(Header())
-    $mainHome.appendChild(PresentationCard())
-    $ProyectsBox.appendChild(GetProyects().Proyects)
-    $mainHome.appendChild($ProyectsBox)
-    $SkillsBox.appendChild(GetProyects().Skills)
-    $mainHome.appendChild($SkillsBox)
     $Fragment.appendChild($mainHome)
     $root.appendChild($Fragment)
+    Router()
 }
 export default App
