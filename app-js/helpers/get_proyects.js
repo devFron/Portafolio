80d1eed33efcee2533b4289866ff53c7dc9323b8
Dev Front <developermater26@gmail.com>
2021-08-28 19:26:55 -0600

    se creo una peticion para poder recuperar la data de los proyectos y asi pintar el componente de cards

---
+++ app-js/helpers/get_proyects.js
@@ -0,0 +1,25 @@
+import CardProyect from "../components/card_proyect.js";
+
+const GetProyects =( ) =>{
+    const $Proyects = document.createElement('section')
+    fetch('http://localhost:8848/app-js/helpers/proyects-skils.json')
+    .then((res)=> res.ok ? res.json() : Promise.reject({status:res.status}))
+    .then((json)=>{
+        let proyects = json.proyects
+        proyects.forEach(el => {
+            $Proyects.appendChild(
+                CardProyect({
+                    id:el.id,
+                    image:el.image,
+                    title:el.title,
+                    description:el.description,
+                    link:el.link
+                })
+            )
+        });
+    })
+    .catch((err)=>{console.log(err.status)})
+
+    return $Proyects
+}
+export default GetProyects
