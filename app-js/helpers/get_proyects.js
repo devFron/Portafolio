dd83e764d3cd08ad18efdcb75286131c937d4cab
Dev Front <developermater26@gmail.com>
2021-08-29 12:27:53 -0600

    se recuperaron los skills y se retorno los proyectso y skills en un objeto

--- app-js/helpers/get_proyects.js
+++ app-js/helpers/get_proyects.js
@@ -1,7 +1,9 @@
 import CardProyect from "../components/card_proyect.js";
+import CardSkills from "../components/card_skills.js";
 
 const GetProyects =( ) =>{
     const $Proyects = document.createElement('section')
+    const $Skills = document.createElement('section')
     fetch('http://localhost:8848/app-js/helpers/proyects-skils.json')
     .then((res)=> res.ok ? res.json() : Promise.reject({status:res.status}))
     .then((json)=>{
@@ -17,9 +19,24 @@ const GetProyects =( ) =>{
                 })
             )
         });
+        let Skills = json.skills
+        Skills.forEach(el => {
+            $Skills.appendChild(
+                CardSkills({
+                    image:el.image,
+                    nameProyect:el.title,
+                    link:el.link
+                })
+            )
+        });
+
     })
     .catch((err)=>{console.log(err.status)})
     $Proyects.classList.add('proyects__box')
-    return $Proyects
+    $Skills.classList.add('skills__box')
+    return {
+        Proyects:$Proyects,
+        Skills:$Skills
+    }
 }
 export default GetProyects
