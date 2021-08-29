0425a9816d713ec50fd747e7b5099ac25ae13eb9
Dev Front <developermater26@gmail.com>
2021-08-28 19:27:31 -0600

    se importo el helper que retorna todo los proyectos

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import Header from './app-js/components/header.js'
 import PresentationCard from './app-js/components/presentation_card.js'
+import GetProyects from './app-js/helpers/get_proyects.js'
 const App =()=>{
     const $root = document.getElementById('root')
     const $Fragment = document.createDocumentFragment()
-
+    const $ProyectsBox = document.createElement('section')
 
 
 
     $Fragment.appendChild(Header())
     $Fragment.appendChild(PresentationCard())
+    $ProyectsBox.appendChild(GetProyects())
+    $Fragment.appendChild($ProyectsBox)
     $root.appendChild($Fragment)
 }
 export default App
