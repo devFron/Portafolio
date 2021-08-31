20c89f56738079d36f4dc8321838e2c70804967b
Dev Front <developermater26@gmail.com>
2021-08-30 20:02:33 -0600

    importo la funcion del toggl para el modo oscuro y se agrego un listender a el dom con el eveto click donde se ejuta dicha funcion y se guarda en el localStorage una llave que contiene el la informacion si esta en modo oscuro o no

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+import { DarkModeToggle } from "./app-js/helpers/dark-mode.js";
 import App from "./App.js";
 
-document.addEventListener('DOMContentLoaded',async(e)=> await App())
-window.addEventListener('hashchange',async(e)=> await App())
+document.addEventListener('DOMContentLoaded',async(e)=>{
+    await App()
+})
+window.addEventListener('hashchange',async(e)=> {
+    await App()
+})
+document.addEventListener('click',(e)=>{
+    if(e.target.matches('.dark-mode__circle')||e.target.matches('.dark-mode__icon')){
+        DarkModeToggle()
+        if(document.querySelector('body').classList.contains('body-dark')){
+            localStorage.setItem('devfron-dark-mode','true')
+        }else{
+            localStorage.setItem('devfron-dark-mode','false')
+        }
+    }
+})
