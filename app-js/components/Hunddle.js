8fbf8558a4f6da210ff27ee668b9240cd6e9bf7f
Dev Front <developermater26@gmail.com>
2021-09-01 00:15:04 -0600

    se importo el menu de skills

--- app-js/components/Hunddle.js
+++ app-js/components/Hunddle.js
@@ -1,6 +1,9 @@
+import MenuSkill from "./Menu_skill.js"
 const HunddleSkill = ()=>{
     const $Hunddle = document.createElement('section')
+    let $pageLink = 'https://www.frontendmentor.io/challenges/huddle-landing-page-with-curved-sections-5ca5ecd01e82137ec91a50f2'
     $Hunddle.classList.add('hunddle__box')
+    $Hunddle.appendChild(MenuSkill($pageLink))
     return $Hunddle
 }
 export default HunddleSkill
