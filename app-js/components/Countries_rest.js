364aac27fccb37c86c93176f1de71045dca6ca61
Dev Front <developermater26@gmail.com>
2021-09-06 13:42:24 -0600

    este componente tendra el la vista de rest-countries

---
+++ app-js/components/Countries_rest.js
@@ -0,0 +1,6 @@
+const CountriesSkill =()=>{
+    const $Countries = document.createElement('section')
+    $Countries.classList.add('countries')
+    return $Countries
+}
+export default CountriesSkill
