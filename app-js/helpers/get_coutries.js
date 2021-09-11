bcbeecaf1f6d1cb3c2db1fdc1b45878ddab42f55
DevFront <developermater26@gmai.com>
2021-09-11 17:42:26 -0600

    se creo el cuerpo del la vista de un pais

--- app-js/helpers/get_coutries.js
+++ app-js/helpers/get_coutries.js
@@ -5,14 +5,15 @@ const Countri =(props)=>{
     const $countryName = document.createElement('p')
     const $countryData = document.createElement('div')
     const $countryDataList = document.createElement('ul')
-
+    const $countryClick = document.createElement('section')
+    $countryClick.classList.add('country-click')
     $country.classList.add('country')
     $countryImage.classList.add('country__image')
     $countryName.classList.add('country__name')
     $countryData.classList.add('country__data')
     $countryDataList.classList.add('country__data__list')
 
-    $country.dataset.id = id
+    $countryClick.dataset.id = name
     $countryImage.src = img
     $countryImage.alt = `Flag ${name}`
     $countryName.textContent = name
@@ -40,6 +41,7 @@ const Countri =(props)=>{
     $countryData.appendChild($countryDataList)
     $country.appendChild($countryImage)
     $country.appendChild($countryData)
+    $country.appendChild($countryClick)
     return $country
 }
 const GetCountries = async(countries)=>{
@@ -84,3 +86,107 @@ const GetCountries = async(countries)=>{
 
 }
 export default GetCountries
+
+export const ACountry = async(name)=>{
+    const $ACountry = document.createElement('section')
+    const $ACountryButton = document.createElement('button')
+    const $ACountryImage = document.createElement('img')
+    const $ACountryName = document.createElement('p')
+    const $ACountryData = document.createElement('article')
+    const $ACountryDataOne = document.createElement('ul')
+    const $ACountryDataTwo = document.createElement('ul')
+    const $ACountryBorderBox = document.createElement('article')
+    const $ACountryBorderTitle = document.createElement('p')
+    const $ACountryBorders = document.createElement('ul')
+
+    $ACountry.classList.add('a-country')
+    $ACountryButton.classList.add('a-country__button')
+    $ACountryImage.classList.add('a-country__image')
+    $ACountryName.classList.add('a-country__name')
+    $ACountryData.classList.add('a-country__data__box')
+    $ACountryDataOne.classList.add('a-country__data')
+    $ACountryDataTwo.classList.add('a-country__data')
+    $ACountryBorderBox.classList.add('a-country__border__box')
+    $ACountryBorderTitle.classList.add('a-country__border__title')
+    $ACountryBorders.classList.add('a-country__border__borders')
+    
+    $ACountryButton.innerHTML = `<i class="fas fa-arrow-left"></i> Back`
+    try {
+        let res = await fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        if(!res.ok) throw res.status
+        let json = await res.json()
+        json.forEach((el)=>{
+            $ACountryName.textContent = el.name
+            $ACountryImage.src = el.flag
+            $ACountryImage.alt = `Flag from ${el.name}`
+            const dataOne = {
+                Native_Name:el.nativeName ? el.nativeName : ' ',
+                Population:el.population ? el.population : ' ',
+                Region:el.region ? el.region : ' ',
+                Sub_Region:el.subregion ? el.subregion : ' ' 
+            }
+            let DataOneEnd = Object.keys(dataOne)
+            for (let i = 0; i < DataOneEnd.length; i++) {
+                const $ACountryDataOneItem = document.createElement('li')
+                const $ACountryDataOneItemTitle = document.createElement('span')
+                const $ACountryDataOneItemDescription = document.createElement('span')
+                $ACountryDataOneItem.classList.add('a-country__data__item')
+                $ACountryDataOneItemTitle.textContent = `${DataOneEnd[i]}:`
+                $ACountryDataOneItemDescription.textContent = dataOne[DataOneEnd[i]]
+                $ACountryDataOneItem.appendChild($ACountryDataOneItemTitle)
+                $ACountryDataOneItem.appendChild($ACountryDataOneItemDescription)
+                $ACountryDataOne.appendChild($ACountryDataOneItem) 
+            }
+
+            const currenciesAll = []
+            el.currencies.forEach((el)=>{currenciesAll.push(el.name)}) 
+
+            const lenguagesAll = []
+            el.languages.forEach((el)=>{lenguagesAll.push(el.name)}) 
+
+            const dataTwo = {
+                Top_Level_Domain:el.topLevelDomain.join(),
+                currencies:currenciesAll.join(),
+                Languages:lenguagesAll.join()
+            }
+            let DataTwoEnd = Object.keys(dataTwo)
+            for (let i = 0; i < DataTwoEnd.length; i++) {
+                const $ACountryDataTwoItem = document.createElement('li')
+                const $ACountryDataTwoItemTitle = document.createElement('span')
+                const $ACountryDataTwoItemDescription = document.createElement('span')
+                $ACountryDataTwoItem.classList.add('a-country__data__item')
+                $ACountryDataTwoItemTitle.textContent = `${DataTwoEnd[i]}:`
+                $ACountryDataTwoItemDescription.textContent = dataTwo[DataTwoEnd[i]]
+                $ACountryDataTwoItem.appendChild($ACountryDataTwoItemTitle)
+                $ACountryDataTwoItem.appendChild($ACountryDataTwoItemDescription)
+                $ACountryDataTwo.appendChild($ACountryDataTwoItem) 
+            }
+            
+            if(el.borders.length > 0){
+                el.borders.forEach((el)=>{
+                    const $borderItem = document.createElement('li')
+                    $borderItem.classList.add('a-country__data__item')
+                    $borderItem.textContent = el
+                    $ACountryBorders.appendChild($borderItem)
+                    $ACountryDataTwo.appendChild($ACountryBorders)
+                })
+            }else{
+                $ACountryBorders.innerHTML = ``
+            }
+        })
+    } catch (err) {
+        alert(`Error ${err}`)
+    }
+
+    $ACountry.appendChild($ACountryButton)
+    $ACountry.appendChild($ACountryImage)
+    $ACountry.appendChild($ACountryName)
+    $ACountryData.appendChild($ACountryDataOne)
+    $ACountryData.appendChild($ACountryDataTwo)
+    $ACountry.appendChild($ACountryData)
+    $ACountryBorderBox.appendChild($ACountryBorderTitle)
+    $ACountryBorderBox.appendChild($ACountryBorders)
+    $ACountry.appendChild($ACountryBorderBox)
+    return $ACountry
+
+}
