559104c8d0da95c6933289c657d4aa30c132e374
devFron <developermater26@gmail.com>
2021-10-19 10:56:18 -0600

    este componente contendra la vista de el 'skill' todo-app

---
+++ app-js/components/Todo_app.js
@@ -0,0 +1,7 @@
+/*
+TODO: Agregar nuevos todos a la lista Marcar todos como completos Eliminar todos de la lista Filtrar por todos activos todos completos Borrar todos los todos completados Alternar modo claro y oscuro Bonificación arrastre y suelte para reordenar los elementos de la lista
+*/
+const TodoApp =()=>{
+
+}
+export default TodoApp
